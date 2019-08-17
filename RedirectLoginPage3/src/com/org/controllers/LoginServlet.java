01467144586a41caae4f074343f539b3b32f9586
Sanjay Kumar Das <43781122+SanjayjKumarDas@users.noreply.github.com>
2019-08-17 22:19:34 +0530

    Redirect Login Page Dynamically

---
+++ RedirectLoginPage3/src/com/org/controllers/LoginServlet.java
@@ -0,0 +1,64 @@
+package com.org.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.org.model.Login;
+import com.org.model.LoginDao;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		
+		Login login = new Login();
+		login.setUsername(request.getParameter("username"));
+		login.setPassword(request.getParameter("password"));
+
+		LoginDao loginDao = new LoginDao();
+
+		if (loginDao.loginCheck(login)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/HomePage.html");
+			rd.forward(request, response);
+		} else {
+			RequestDispatcher rd = request.getRequestDispatcher("/ErrorPage.html");
+			rd.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ RedirectLoginPage3/src/com/org/model/LoginDao.java
@@ -0,0 +1,9 @@
+package com.org.model;
+
+public class LoginDao {
+
+	public boolean loginCheck(Login login) {
+		return true;
+	}
+
+}
